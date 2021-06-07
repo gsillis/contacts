dd5c239408404ff9b151b34147083d3c4d03b941
Gabriela Sillis <gabydias47@gmail.com>
2021-06-07 17:21:40 -0300

    initial commit

---
+++ Agenda/Alunos/Controller/AlunoViewController.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+class AlunoViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var viewImagemAluno: UIView!
+    @IBOutlet weak var imageAluno: UIImageView!
+    @IBOutlet weak var buttonFoto: UIButton!
+    @IBOutlet weak var scrollViewPrincipal: UIScrollView!
+    
+    @IBOutlet weak var textFieldNome: UITextField!
+    @IBOutlet weak var textFieldEndereco: UITextField!
+    @IBOutlet weak var textFieldTelefone: UITextField!
+    @IBOutlet weak var textFieldSite: UITextField!
+    @IBOutlet weak var textFieldNota: UITextField!
+    
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.arredondaView()
+        NotificationCenter.default.addObserver(self, selector: #selector(aumentarScrollView(_:)), name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - Métodos
+    
+    func arredondaView() {
+        self.viewImagemAluno.layer.cornerRadius = self.viewImagemAluno.frame.width / 2
+        self.viewImagemAluno.layer.borderWidth = 1
+        self.viewImagemAluno.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @objc func aumentarScrollView(_ notification:Notification) {
+        self.scrollViewPrincipal.contentSize = CGSize(width: self.scrollViewPrincipal.frame.width, height: self.scrollViewPrincipal.frame.height + self.scrollViewPrincipal.frame.height/2)
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func buttonFoto(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let multimidia = UIImagePickerController()
+            
+            multimidia.sourceType = .camera
+            self.present(multimidia, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func stepperNota(_ sender: UIStepper) {
+        self.textFieldNota.text = "\(sender.value)"
+    }
+    
+    
+}
---
+++ Agenda/Home/View/HomeTableViewCell.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var imageAluno: UIImageView!
+    @IBOutlet weak var labelNomeDoAluno: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
